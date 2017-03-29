782eb0d9ccbde2794897e219a2bf59201ea199b5
Shikanime <shikanime@hotmail.fr>
2017-03-29 17:01:34 +0200

    modified:   src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -7,6 +7,40 @@
 
 function parseStream(codeTokenized, callback) {
 
+
+    /**
+     * Parse the code
+     */
+    function parse_code() {
+        let code = [];
+
+        while (!codeTokenized.end_of_file()) {
+            code.push(parse_expression());
+            if (!codeTokenized.end_of_file()) skip_punctuation(";");
+        }
+    }
+
+    function parse_if() {
+        skip_keyword("if");
+
+        var condition = parse_expression();
+        if (!is_punctuation("{")) skip_keyword("alors");
+        var then_content = parse_expression();
+        var output = {
+            type: "if",
+            condition: condition,
+            if_content: then_content
+        };
+
+        // Detect if there is another content
+        if (is_keyword("else")) {
+            input.next_char();
+            output.else_content = parse_expression();
+        };
+
+        return output;
+    }
+
     /**
      * Read function content
      * 
@@ -23,35 +57,28 @@ function parseStream(codeTokenized, callback) {
     /**
      * Parse function content
      * 
-     * @param {char} start_char 
-     * @param {char} stop_char 
+     * @param {char} begin_char 
+     * @param {char} end_char 
      * @param {char} separator_char 
      * @param {function} parse 
      */
-    function parse_container(start_char, stop_char, separator_char, parse) {
+    function parse_container(begin_char, end_char, separator_char, parse) {
         let argument = [];
         let first = true;
 
-        skip_container(start);
+        skip_punctuation(begin_char);
         while (!codeTokenized.end_of_file()) {
-            if (is_container(stop)) break;
+            if (is_punctuation(end_char)) break;
 
             // Skip empty function arguments
             if (first) first = false;
-            else skip_container(separator_char);
+            else skip_punctuation(separator_char);
 
-            if (is_container(stop)) break;
+            if (is_punctuation(end_char)) break;
             argument.push(parser());
         }
-        skip_container(stop);
+        skip_punctuation(end_char);
 
         return argument;
-    }
-
-    function parse_code() {
-        let code = [];
-        while (!codeTokenized.end_of_file()) {
-            code.push(parse_expression())
-        }
     }
 }
