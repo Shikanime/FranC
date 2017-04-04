dcdb26e39f47b15ff5aa8375bebf222fa8b6d5f5
Shikanime <shikanime@hotmail.fr>
2017-04-04 22:49:50 +0200

    new file:   src/test.js

---
+++ src/test.js
@@ -0,0 +1,10 @@
+const francais = require("./francais");
+
+codeEntry = "si foo alors bar ou baz;";
+
+let treatedCode = francais.interprete(codeEntry);
+
+// Debugger
+//francais.debug("explorer", codeEntry);
+//francais.debug("lexer", codeEntry);
+francais.debug("parser", codeEntry);
